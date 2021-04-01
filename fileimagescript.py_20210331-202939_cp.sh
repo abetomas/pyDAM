1bf58db3e8a393d82e5992d4c72cc2b415472bb1
abetomas <abetomas@users.noreply.github.com>
2021-03-31 20:45:23 -0400

    Add files via upload
    Sample files : 
    - log file of actions taken on each file
    - shell script

---
+++ fileimagescript.py_20210331-202939_cp.sh
@@ -0,0 +1,24 @@
+mkdir -v ./tmp
+mkdir -v ./tmp/No-Capture-Date
+mkdir -v ./tmp/2019
+mkdir -v ./tmp/2019/2019-02-09
+cp -pnv ./test/IMG_0095.HEIC ./tmp/2019/2019-02-09
+mkdir -v ./tmp/2021
+mkdir -v ./tmp/2021/2021-01-23
+cp -pnv ./test/IMG_7937.HEIC ./tmp/2021/2021-01-23
+cp -pnv ./test/IMG_7937a.HEIC ./tmp/2021/2021-01-23
+mkdir -v ./tmp/2019/2019-01-27
+cp -pnv ./test/subdir-1/IMG_0047.dng ./tmp/2019/2019-01-27
+mkdir -v ./tmp/2019/2019-09-05
+cp -pnv ./test/subdir-1/IMG_0613.m4v ./tmp/2019/2019-09-05
+mkdir -v ./tmp/2017
+mkdir -v ./tmp/2017/2017-09-11
+cp -pnv ./test/subdir-1/IMG_2646.jpg ./tmp/2017/2017-09-11
+cp -pnv ./test/subdir-1/IMG_4445.PNG ./tmp/No-Capture-Date/
+mkdir -v ./tmp/2020
+mkdir -v ./tmp/2020/2020-02-11
+cp -pnv ./test/subdir-1/IMG_4449.MOV ./tmp/2020/2020-02-11
+mkdir -v ./tmp/2017/2017-04-14
+cp -pnv ./test/subdir-2/20170414_184453.jpg ./tmp/2017/2017-04-14
+cp -pnv ./test/subdir-2/20170414_184515.jpg ./tmp/2017/2017-04-14
+cp -pnv ./test/subdir-2/IMG_2646.jpg ./tmp/2017/2017-09-11
